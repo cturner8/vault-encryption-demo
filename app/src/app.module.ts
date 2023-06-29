c0150888393744a9d957b9bc2d95769369704813
cameron turner <17177161+cturner8@users.noreply.github.com>
2023-06-29 20:07:33 +0000

    add initial messages controller

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatsModule } from './chats/chats.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [ChatsModule],
+  imports: [ChatsModule, MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
