ea696601878dc3be07933ae3ad599cdd01681515
cameron turner <17177161+cturner8@users.noreply.github.com>
2023-06-29 20:12:44 +0000

    add initial user controller

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatsModule } from './chats/chats.module';
 import { MessagesModule } from './messages/messages.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ChatsModule, MessagesModule],
+  imports: [ChatsModule, MessagesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
