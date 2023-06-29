f68e47f0f017f0231dba41f50ac35d57c3a2afe6
cameron turner <17177161+cturner8@users.noreply.github.com>
2023-06-29 21:11:20 +0000

    initial user schema, reorganise nest modules

--- app/src/modules/app/app.controller.ts
+++ app/src/modules/app/app.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor() {}
-
   @Get()
   root(): string {
     return 'API is running';
--- app/src/modules/app/app.module.ts
+++ app/src/modules/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ChatsModule } from '../chats/chats.module';
+import { MessagesModule } from '../messages/messages.module';
+import { UsersModule } from '../users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ChatsModule } from './chats/chats.module';
-import { MessagesModule } from './messages/messages.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ChatsModule, MessagesModule, UsersModule],
